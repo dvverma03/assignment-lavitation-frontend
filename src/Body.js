32cb8fe4313ddc3a0eb29437a32dd4242a4631f8
dvverma03 <116081426+dvverma03@users.noreply.github.com>
2024-03-15 13:52:16 +0530

    Update Body.js

--- src/Body.js
+++ src/Body.js
@@ -1,9 +1,8 @@
-
 import { useDispatch, useSelector } from 'react-redux';
 import Cards from './cards';
 import { addInvoice, removeInvoice } from './utils/invoiceslice';
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { addId } from "./utils/idSlice";
 
@@ -11,30 +10,37 @@ const Body = () => {
 
   const dispatch = useDispatch()
   const navigate = useNavigate()
-  let data = []; 
-  data = useSelector(store => store.invoice.invoice);
+  let data = useSelector(store => store.invoice.invoice);
 
   useEffect(() => {
-    const token = document.cookie;
-    const token1 = token.substring(6,)
-    if (!data.length) {
-      axios.post("https://assignment-lavitation-backend.vercel.app/browse", { token1 })
-      .then((res) => {
-        const userId = res.data._id
-        console.log(res)
-        dispatch(addId(userId))
+    const fetchData = async () => {
+      try {
+        const email= localStorage.getItem("email")
         dispatch(removeInvoice())
-        {
-          res?.data?.products.length > 0 && res.data.products.map((e) => {
-
-            dispatch(addInvoice(e))
-          })
+        const res = await axios
+        .post("https://assignment-lavitation-backend.vercel.app/browse", { email });
+        
+        // .post("http://localhost:1234/browse", {email })
+        const userId = res.data._id;
+        dispatch(addId(userId));
+        console.log(data)
+        dispatch(removeInvoice());
+        if (res.data.products.length > 0) {
+          res.data.products.forEach((e) => {
+            dispatch(addInvoice(e));
+            console.log(e)
+          });
         }
-      })
-      .catch((err) => console.log(err));
+        navigate("/invoice");
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    if (!data.length) {
+      fetchData();
     }
-    token && navigate("/invoice")
-  }, [data]);
+  }, []);
 
   const invoice = useSelector((store) => store?.invoice?.invoice);
 
@@ -57,3 +63,95 @@ const Body = () => {
 };
 
 export default Body;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { useDispatch, useSelector } from 'react-redux';
+// import Cards from './cards';
+// import { addInvoice, removeInvoice } from './utils/invoiceslice';
+// import axios from 'axios';
+// import { useEffect, useState } from 'react';
+// import { useNavigate } from 'react-router-dom';
+// import { addId } from "./utils/idSlice";
+
+// const Body = () => {
+
+//   const dispatch = useDispatch()
+//   const navigate = useNavigate()
+//   let data = []; 
+//   data = useSelector(store => store.invoice.invoice);
+
+//   useEffect(() => {
+//     const token = document.cookie;
+//     const token1 = token.substring(6,)
+//     if (!data.length) {
+//       axios.post("https://assignment-lavitation-backend.vercel.app/browse", { token1 })
+//       .then((res) => {
+//         const userId = res.data._id
+//         console.log(res)
+//         dispatch(addId(userId))
+//         dispatch(removeInvoice())
+//         {
+//           res?.data?.products.length > 0 && res.data.products.map((e) => {
+
+//             dispatch(addInvoice(e))
+//           })
+//         }
+//       })
+//       .catch((err) => console.log(err));
+//     }
+//     token && navigate("/invoice")
+//   }, [data]);
+
+//   const invoice = useSelector((store) => store?.invoice?.invoice);
+
+//   return (
+//     <div className="flex flex-col min-h-[750px]">
+//       <div>
+//         <ul className='flex justify-between mx-4 py-4'>
+//           <li className='text-xl font-bold w-1/5'>Product</li>
+//           <li className='text-xl font-bold w-1/5'>Qyt</li>
+//           <li className='text-xl font-bold w-1/5'>Rate</li>
+//           <li className='text-xl font-bold w-1/5'>Total</li>
+//         </ul>
+//         <hr />
+//         <Cards invoice={invoice} />
+//         <hr />
+//       </div>
+
+//     </div>
+//   );
+// };
+
+// export default Body;
